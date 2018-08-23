70436b0b289a9c29ee496d5fb1581fcee6e6b9a8
POLEO DAVALOS FRANCISCO <x268815@gruposantander.es>
2018-08-23 10:46:48 +0200

    chore(playground2): added another playground for testing purposes.

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,5 @@
 const stream = require('./stream');
 
-const cmd = `npx ../../node_modules/.bin/rollup -c ../../rollup.config.js ${process.env.WATCH}`;
+const cmd = `npx ../../node_modules/.bin/rollup -c ../../rollup.config.js ${process.env.WATCH || ''}`;
 
 stream(cmd);
