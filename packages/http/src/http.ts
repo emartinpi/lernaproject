64979fcfbcb9fe628fbf99297d65c0f44a126ee6
Edu Martin <emartinpi@gmail.com>
2018-09-03 16:55:02 +0200

    añade addprovider

--- packages/http/src/http.ts
+++ packages/http/src/http.ts
@@ -14,14 +14,13 @@ interface Client {
   secret: string;
 }
 
-type Factory<T> = () => T; // El tipo generico T haria referencia a una Api como HttpApi
-
-type FactoryBuilder<F> = (factory: F) => F;
-type FactoryBuilder1<F> = (param1: any, factory: F) => F;
-type FactoryBuilder2<F> = (param1: any, param2: any, factory: F) => F;
+type Factory<A> = () => A;
+// type FactoryBuilder0Promise<A> = () => Promise<A>;
+type FactoryBuilder1Promise<A> = (param1: any, factory: Factory<A>) => Promise<Factory<A>>;
+// type FactoryBuilder2<F> = (param1: any, param2: any, factory: F) => F;
 
 let darwinProvider: any; // libreria a usar para hacer las llamadas AJAX
-
+darwinProvider = undefined;
 const darwinHttpFactory: Factory<HttpApi> = () => {
   const warning = () => console.log(
     '%cWarning => %cdarwinHttpFactory(): %cSe necesita configurar proveedor...',
@@ -31,77 +30,78 @@ const darwinHttpFactory: Factory<HttpApi> = () => {
   );
 
   return {
-    get(...args) {
+    get(...args: any[]) {
       return darwinProvider ? darwinProvider.get(...args) : warning();
     },
-    post(...args) {
+    post(...args: any[]) {
       return darwinProvider ? darwinProvider.post(...args) : warning();
     },
   };
 };
 
-const addProvider: FactoryBuilder1<Factory<HttpApi>> =
+const addProvider: FactoryBuilder1Promise<HttpApi> =
   (provider: any, factory: Factory<HttpApi>) => {
-    return new Proxy(factory, {
-      apply(target, thisArg, argumentsList): HttpApi {
-        const api = target();
-        darwinProvider = provider;
-        return {
-          get(...args: any[]) {
-            return api.get.apply(api, args);
-          },
-          post(...args: any[]) {
-            return api.post.apply(api, args);
-          },
-        };
-      },
-    });
+    return Promise.resolve(
+      new Proxy(factory, {
+        apply(target, thisArg, argumentsList) {
+          const api = target();
+          darwinProvider = provider;
+          return {
+            get(...args: any[]) {
+              return api.get.apply(api, args);
+            },
+            post(...args: any[]) {
+              return api.post.apply(api, args);
+            },
+          };
+        },
+      }),
+    );
   };
 
-const configHeaders: FactoryBuilder1<Factory<HttpApi>> =
-  (headers: Header, factory: Factory<HttpApi>) => new Proxy(factory, {
-    apply(target, thisArg, argumentsList): HttpApi {
-      const api = target(); // obtiene la api que hubiera configurada hasta este momento
-      return {
-        get(...args: any[]) {
-          const allHeaders: Header = Object.assign({}, args[1] ? args[1].headers : {}, headers);
-          return api.get(args[0], { headers: allHeaders });
-        },
-        post(...args: any[]) {
-          const allHeaders: Header = Object.assign({}, args[2] ? args[2].headers : {}, headers);
-          return api.post(args[0], args[1], { headers: allHeaders });
-        },
-      };
-    },
-  });
+// const configHeaders: FactoryBuilder1<Factory<HttpApi>> =
+//   (headers: Header, factory: Factory<HttpApi>) => new Proxy(factory, {
+//     apply(target, thisArg, argumentsList): HttpApi {
+//       const api = target(); // obtiene la api que hubiera configurada hasta este momento
+//       return {
+//         get(...args: any[]) {
+//           const allHeaders: Header = Object.assign({}, args[1] ? args[1].headers : {}, headers);
+//           return api.get(args[0], { headers: allHeaders });
+//         },
+//         post(...args: any[]) {
+//           const allHeaders: Header = Object.assign({}, args[2] ? args[2].headers : {}, headers);
+//           return api.post(args[0], args[1], { headers: allHeaders });
+//         },
+//       };
+//     },
+//   });
 
-const configToken: FactoryBuilder2<Factory<HttpApi>> =
-(tokenServiceUrl: string, client: Client, factory: Factory<HttpApi>) => new Proxy(factory, {
-  apply(target, thisArg, argumentsList): HttpApi {
-    // llamada a post para obtener token
-    const api = target(); // obtiene la api que hubiera configurada hasta este momento
-    api.post(tokenServiceUrl, 'grant_type=client_credentials', {
-      headers: {
-        // tslint:disable-next-line
-        'Authorization': `Basic ${btoa(client.id + ':' + client.secret)}`,
-      },
-    }).then((response: any) => {
-      console.log(response);
-    }).catch((err: any) => {
-      console.log(err);
-    });
+// const configToken: FactoryBuilder2<Factory<HttpApi>> =
+// (tokenServiceUrl: string, client: Client, factory: Factory<HttpApi>) => new Proxy(factory, {
+//   apply(target, thisArg, argumentsList): HttpApi {
+//     // llamada a post para obtener token
+//     const api = target(); // obtiene la api que hubiera configurada hasta este momento
+//     api.post(tokenServiceUrl, 'grant_type=client_credentials', {
+//       headers: {
+//         Authorization: `Basic ${btoa(client.id + ':' + client.secret)}`,
+//       },
+//     }).then((response: any) => {
+//       console.log(response);
+//     }).catch((err: any) => {
+//       console.log(err);
+//     });s
 
-    return api;
-  },
-});
+//     return api;
+//   },
+// });
 
-const addProviderCurrified = curry(addProvider);
-const configHeadersCurrified = curry(configHeaders);
-const configTokenCurrified = curry(configToken);
+const addProviderCurrified = addProvider;
+// const configHeadersCurrified = curry(configHeaders);
+// const configTokenCurrified = curry(configToken);
 
 export {
   darwinHttpFactory,
   addProviderCurrified as addProvider,
-  configHeadersCurrified as configHeaders,
-  configTokenCurrified as configToken,
+  // configHeadersCurrified as configHeaders,
+  // configTokenCurrified as configToken,
 };
--- playgrounds/native/src/index.ts
+++ playgrounds/native/src/index.ts
@@ -1,4 +1,4 @@
-import { darwinHttpFactory, addProvider, configHeaders, configToken } from '@monorepo/http';
+import { darwinHttpFactory, addProvider } from '@monorepo/http';
 import { pipe } from 'ramda';
 import axios from 'axios';
 
@@ -9,16 +9,26 @@ import axios from 'axios';
 //   'X-Requested-With': 'DarwinXMLHttpRequest',
 // })(darwinHttp1);
 
-const allFactoryConfig = pipe(
-  addProvider(axios),
-  configHeaders({ 'X-Requested-With': 'DarwinXMLHttpRequest' }),
-  configToken('https://accounts.spotify.com/api/token', {
-    id: '6aa367588666404db1162e5ba087998d',
-    secret: '2b6f5eef0cb54ea496a1710ff743bcd7',
-  }),
-);
+// const allFactoryConfig = pipe(
+//   addProvider(axios),
+//   configHeaders({ 'X-Requested-With': 'DarwinXMLHttpRequest' }),
+//   configToken('https://accounts.spotify.com/api/token', {
+//     id: '6aa367588666404db1162e5ba087998d',
+//     secret: '2b6f5eef0cb54ea496a1710ff743bcd7',
+//   }) // tslint:disable-line
+// );
 
-const factoryBuilder = allFactoryConfig(darwinHttpFactory);
-const httpApi = factoryBuilder();
+// const factoryBuilder = allFactoryConfig(darwinHttpFactory);
+// const httpApi = factoryBuilder();
 
-httpApi.get('/users');
+// httpApi.get('/users');
+
+(async () => {
+
+  // const httpApi1 =  darwinHttpFactory();
+
+  const factory1 = await addProvider(axios, darwinHttpFactory);
+
+  const httpApi = factory1();
+  httpApi.get('/user');
+})();
