04cf99a854c1dca7ab49238e1bae28b26d59b757
Edu Martin <emartinpi@gmail.com>
2018-09-04 17:02:43 +0200

    añade una nueva api configurada de la anterior

--- playgrounds/native/src/index.ts
+++ playgrounds/native/src/index.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 (async () => {
   const darwinHttpFactoryBuilder = pipeP(
     addProvider(axios),
-    configHeaders({ 'X-Requested-With': 'DarwinXMLHttpRequest' }),
+    configHeaders({ 'X-Requested-With': 'DarwinXMLHttpRequest-X' }),
     configToken('https://accounts.spotify.com/api/token', {
       id: '6aa367588666404db1162e5ba087998d',
       secret: '2b6f5eef0cb54ea496a1710ff743bcd7',
@@ -16,4 +16,10 @@ import axios from 'axios';
   const httpApi = darwinHttpFactoryConfigured();
   const playlist = await httpApi.get('https://api.spotify.com/v1/browse/featured-playlists');
   console.log(playlist);
+
+  const darwinHttpFactoryBuilder2 =
+  await configHeaders({ 'Y-Requested-With': 'DarwinXMLHttpRequest-Y' }, darwinHttpFactoryConfigured);
+
+  const httpApi2 = darwinHttpFactoryBuilder2();
+  httpApi2.get('/user');
 })();
