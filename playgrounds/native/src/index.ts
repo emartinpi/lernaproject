a1d09bc2355d4ecd4004312b6588fe63f6b4d14b
Edu Martin <emartinpi@gmail.com>
2018-09-06 16:44:02 +0200

    native

--- playgrounds/native/src/index.ts
+++ playgrounds/native/src/index.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 (async () => {
   const darwinHttpFactoryBuilder = pipeP(
     addProvider(axios),
-    configHeaders({ 'X-Requested-With': 'DarwinXMLHttpRequest-X' }),
+    // configHeaders({ 'X-Requested-With': 'DarwinXMLHttpRequest-X' }),
     configToken('https://accounts.spotify.com/api/token', {
       id: '6aa367588666404db1162e5ba087998d',
       secret: '2b6f5eef0cb54ea496a1710ff743bcd7',
@@ -15,7 +15,7 @@ import axios from 'axios';
   const darwinHttpFactoryConfigured = await darwinHttpFactoryBuilder(darwinHttpFactory);
   const httpApi = darwinHttpFactoryConfigured();
   const playlist = await httpApi.get('https://api.spotify.com/v1/browse/featured-playlists');
-  console.log(playlist);
+  console.log(playlist.data);
 
   const darwinHttpFactoryBuilder2 =
   await configHeaders({ 'Y-Requested-With': 'DarwinXMLHttpRequest-Y' }, darwinHttpFactoryConfigured);
