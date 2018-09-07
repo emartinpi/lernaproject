73073ad0374cac9f363f184f4f63de66f5489f67
Edu Martin <emartinpi@gmail.com>
2018-09-07 11:20:31 +0200

    demas funciones de configuracion compatibles con promesas y observables

--- packages/http/src/http.ts
+++ packages/http/src/http.ts
@@ -16,8 +16,8 @@ interface Client {
 }
 
 type Factory<A> = () => A;
-type FactoryBuilder1Promise<P, A> = (p1: P, factory: Factory<A>) => Promise<Factory<A>>;
-type FactoryBuilder2Promise<P, Q, A> = (p1: P, p2: Q, factory:  Factory<A>) => Promise<Factory<A>>;
+// type FactoryBuilder1Promise<P, A> = (p1: P, factory: Factory<A>) => Promise<Factory<A>>;
+// type FactoryBuilder2Promise<P, Q, A> = (p1: P, p2: Q, factory:  Factory<A>) => Promise<Factory<A>>;
 
 let darwinProvider: any; // libreria a usar para hacer las llamadas AJAX
 
@@ -45,65 +45,67 @@ const addProvider: <R, S extends R>(p1: HttpApi<R>, factory: Factory<HttpApi<S>>
     return Promise.resolve(factory);
   };
 
-const configHeaders: FactoryBuilder1Promise<Header, HttpApi> = (headers, factory) => {
-  return Promise.resolve(
-    new Proxy(factory, {
-      apply(target, thisArg, argumentsList): HttpApi {
-        const api = target(); // obtiene la api que hubiera configurada hasta este momento
-        return {
-          get(...args: any[]) {
-            const allHeaders: Header = Object.assign({}, args[1] ? args[1].headers : {}, headers);
-            return api.get(args[0], { headers: allHeaders });
-          },
-          post(...args: any[]) {
-            const allHeaders: Header = Object.assign({}, args[2] ? args[2].headers : {}, headers);
-            return api.post(args[0], args[1], { headers: allHeaders });
-          },
-        };
-      },
-    }),
-  );
-};
+const configHeaders: <R>(p1: Header, factory: Factory<HttpApi<R>>) => Promise<Factory<HttpApi<R>>> =
+  (headers, factory) => {
+    return Promise.resolve(
+      new Proxy(factory, {
+        apply(target, thisArg, argumentsList) {
+          const api = target(); // obtiene la api que hubiera configurada hasta este momento
+          return {
+            get(...args: any[]) {
+              const allHeaders: Header = Object.assign({}, args[1] ? args[1].headers : {}, headers);
+              return api.get(args[0], { headers: allHeaders });
+            },
+            post(...args: any[]) {
+              const allHeaders: Header = Object.assign({}, args[2] ? args[2].headers : {}, headers);
+              return api.post(args[0], args[1], { headers: allHeaders });
+            },
+          };
+        },
+      }),
+    );
+  };
 
-const configToken: FactoryBuilder2Promise<string, Client, HttpApi> = (tokenServiceUrl, client, factory) => {
-  return new Promise<Factory<HttpApi>>((resolve, reject) => {
-    const api = factory();
-    const post = api.post(tokenServiceUrl, 'grant_type=client_credentials', {
-      headers: {
-        Authorization: `Basic ${btoa(`${client.id}:${client.secret}`)}`,
-      },
+const configToken: <R extends any>(p1: string, p2: Client, factory: Factory<HttpApi<R>>) => Promise<Factory<HttpApi<R>>> =
+  (tokenServiceUrl, client, factory) => {
+    return new Promise((resolve, reject) => {
+      const api = factory();
+      const post = api.post(tokenServiceUrl, 'grant_type=client_credentials', {
+        headers: {
+          Authorization: `Basic ${btoa(`${client.id}:${client.secret}`)}`,
+        },
+      });
+
+      const callbackOK = (res: any) => {
+        const token = res.data;
+        resolve(
+          new Proxy(factory, {
+            apply(target, thisArg, argumentsList) {
+              const tokenHeader: Header = {
+                Authorization: `Bearer ${token.access_token}`,
+              };
+              return {
+                get(...args: any[]) {
+                  const allHeaders: Header = Object.assign({}, args[1] ? args[1].headers : {}, tokenHeader);
+                  return api.get(args[0], { headers: allHeaders });
+                },
+                post(...args: any[]) {
+                  const allHeaders: Header = Object.assign({}, args[2] ? args[2].headers : {}, tokenHeader);
+                  return api.post(args[0], args[1], { headers: allHeaders });
+                },
+              };
+            },
+          }),
+        );
+      };
+
+      const callbackKO = (err: any) => { reject(err); };
+
+      isObservable(post) ?
+        post.subscribe(callbackOK, callbackKO) :
+        post.then(callbackOK, callbackKO);
     });
-
-    const callbackOK = (res: any) => {
-      const token = res.data;
-      resolve(
-        new Proxy(factory, {
-          apply(target, thisArg, argumentsList): HttpApi {
-            const tokenHeader: Header = {
-              Authorization: `Bearer ${token.access_token}`,
-            };
-            return {
-              get(...args: any[]) {
-                const allHeaders: Header = Object.assign({}, args[1] ? args[1].headers : {}, tokenHeader);
-                return api.get(args[0], { headers: allHeaders });
-              },
-              post(...args: any[]) {
-                const allHeaders: Header = Object.assign({}, args[2] ? args[2].headers : {}, tokenHeader);
-                return api.post(args[0], args[1], { headers: allHeaders });
-              },
-            };
-          },
-        }),
-      );
-    };
-
-    const callbackKO = (err: any) => { reject(err); };
-
-    isObservable(post) ?
-      post.subscribe(callbackOK, callbackKO) :
-      post.then(callbackOK, callbackKO);
-  });
-};
+  };
 
 const addProviderCurrified = curry(addProvider);
 const configHeadersCurrified = curry(configHeaders); // tslint:disable-line
@@ -119,7 +121,6 @@ export {
 /**
  * Utils
  */
-function isObservable(param: Observable<any> | Promise<any>): param is Observable<any> {
+function isObservable(param: any): param is Observable<any> {
   return (<Observable<any>>param).subscribe !== undefined;
 }
-
